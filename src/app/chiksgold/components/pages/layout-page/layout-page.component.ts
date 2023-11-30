8b6aee8e944618bf4b02d69653a70fb33c4dd156
jeremy <j.arias@motorcredito.com.do>
2023-11-29 22:22:18 -0400

    List

--- src/app/chiksgold/components/pages/layout-page/layout-page.component.ts
+++ src/app/chiksgold/components/pages/layout-page/layout-page.component.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
   styleUrls: ['./layout-page.component.css']
 })
 export class LayoutPageComponent {
-  mostrarIcono: boolean = false;
-  mostrarIconoUser: boolean = false;
+
+
   mostrarIconoCurrency: boolean = false;
   mostrarIconoItems: boolean = false;
   mostrarIconoAccount: boolean = false;
   mostrarIconoServices: boolean = false;
   mostrarIconoSwap: boolean = false;
   mostrarIconoSell: boolean = false;
-
-
+  mostrarIconoUsd: boolean = false;
 
 }
