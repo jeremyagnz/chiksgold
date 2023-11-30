edc5113c8f618e61f2263245ecc5f168f464409f
jeremy <j.arias@motorcredito.com.do>
2023-11-29 23:55:29 -0400

    Affing card component layout

---
+++ src/app/chiksgold/components/pages/cards-layout/cards-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-layout',
+  templateUrl: './cards-layout.component.html',
+  styleUrls: ['./cards-layout.component.css']
+})
+export class CardsLayoutComponent {
+
+}
--- src/app/chiksgold/components/pages/layout-page/layout-page.component.ts
+++ src/app/chiksgold/components/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 
 @Component({
@@ -16,5 +16,12 @@ export class LayoutPageComponent {
   mostrarIconoSwap: boolean = false;
   mostrarIconoSell: boolean = false;
   mostrarIconoUsd: boolean = false;
+
+  isScrolled = false;
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    this.isScrolled = window.scrollY > 50;
+  }
 
 }
